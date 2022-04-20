d90c9fd94929d6d8235762febab2aa0947b499da
nitinkadam70 <99539536+nitinkadam70@users.noreply.github.com>
2022-04-20 09:18:02 +0530

    commite

--- my-app/src/Components/AddHouse/AddHouse.jsx
+++ my-app/src/Components/AddHouse/AddHouse.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react";
 
 
+//easienst way to create uniqe id => id: new Date().getTime().toString()
 export const AddHouse = () => {
 
   const [formData, setFromData] = useState({})
-  const [str, setStr] = useState()
 
   const handleChange = (e) => {
 
     let inputName = e.target.className
-    console.log(e.target.value)
 
     if (e.target.type === "checkbox") {
       setFromData({
         ...formData,
-        [inputName]: setStr(e.target.checked),
+        [inputName]: e.target.checked,
       });
     }
     else {
       setFromData({
         ...formData,
-        [inputName]: setStr(e.target.value)
+        [inputName]:e.target.value,
       })
 
     }
@@ -32,15 +31,16 @@ export const AddHouse = () => {
   }
 
   const Post = async (formData) => {
+
+    const newData = {...formData , id: new Date().getTime().toString()}
+
     let res = fetch(`http://localhost:8080/houses`, {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
-        formData,
-        id: formData.length + 1
+        ...newData
       })
     })
-    let data = await res.json();
   }
 
 
@@ -50,30 +50,30 @@ export const AddHouse = () => {
     <div className="addHouseContainer">
       <form onSubmit={handleSubmit}>
         <label>name</label>
-        <input type="text" className="name" value={str} required onChange={handleChange} />
+        <input type="text" className="name" value={formData.className} required onChange={handleChange} />
         <br />
         <label>ownerName</label>
-        <input value={str} type="text" className="ownerName" required onChange={handleChange} />
+        <input value={formData.className} type="text" className="ownerName" required onChange={handleChange} />
         <br />
         <label>address</label>
-        <input value={str} type="text" className="address" required onChange={handleChange} />
+        <input value={formData.className} type="text" className="address" required onChange={handleChange} />
         <br />
         <label>areaCode</label>
-        <input value={str} type="text" className="areaCode" required onChange={handleChange} />
+        <input value={formData.className} type="text" className="areaCode" required onChange={handleChange} />
         <br />
         <label>rent</label>
-        <input value={str} type="text" className="rent" required onChange={handleChange} />
+        <input value={formData.className} type="text" className="rent" required onChange={handleChange} />
         <br />
         <label>preferredTenant</label>
         <br />
         <label>bachelor</label>
-        <input checked={str} type="checkbox" className="bachelor" onChange={handleChange} />
+        <input checked={formData.className} type="checkbox" className="bachelor" onChange={handleChange} />
         <br />
         <label>married</label>
-        <input checked={str} type="checkbox" className="married" onChange={handleChange} />
+        <input checked={formData.className} type="checkbox" className="married" onChange={handleChange} />
         <br />
         <label>image</label>
-        <input value={str} type="text" className="image" required onChange={handleChange} />
+        <input value={formData.className} type="text" className="image" required onChange={handleChange} />
         <br />
         <input className="submitBtn" type="submit" />
       </form>
